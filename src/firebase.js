ea56d4bb4b1c2b32d69ce57832b37a1aa4a19a04
shibam17 <shibam.dipu@gmail.com>
2021-05-18 12:26:23 +0530

    added firebases

---
+++ src/firebase.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD2o6NDrL7xa-NxgpzrTOBY8IEPJPdJjpY",
+  authDomain: "linkedin-build.firebaseapp.com",
+  projectId: "linkedin-build",
+  storageBucket: "linkedin-build.appspot.com",
+  messagingSenderId: "421362128465",
+  appId: "1:421362128465:web:c75f1aac64ac526955ca70",
+  measurementId: "G-W6ZH9ZMJ7C",
+};
+
+const firebaseSpp = firebase.initializeApp(firebaseConfig());
+
+const db = firebaseSpp.firestore();
+
+const auth = firebase.auth();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+const storage = firebase.storage();
+
+export {auth, provider, storage};
+
+export default db;
