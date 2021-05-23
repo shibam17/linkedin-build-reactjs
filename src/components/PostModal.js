f841da9c4f30ad0c9c47a73f3306aaeae2e8276f
shibam17 <shibam.dipu@gmail.com>
2021-05-23 12:37:47 +0530

    added modal funxtion

--- src/components/PostModal.js
+++ src/components/PostModal.js
@@ -241,6 +241,11 @@ const Editor = styled.div`
   }
 `;
 
-const UploadImage = styled.div``;
+const UploadImage = styled.div`
+text-align: center;
+img{
+  width: 100%;
+}
+`;
 
 export default PostModal;
