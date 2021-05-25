3eb801980cb1a1c60b7082f54e43c0d7d66a5282
shibam17 <shibam.dipu@gmail.com>
2021-05-25 09:10:49 +0530

    added DB

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
-import { auth, provider } from "../firebase";
+import { auth, provider, storage } from "../firebase";
 import { SET_USER } from "../actions/actionType";
+import db from "../firebase";
 
 export const setUser = (payload) => ({
   type: SET_USER,
@@ -30,10 +31,51 @@ export function getUserAuth() {
 
 export function signOutAPI() {
   return (dispatch) => {
-    auth.signOut().then(() => {
-      dispatch(setUser(null));
-    }).catch((error)=>{
-      console.log(error);
-    });
+    auth
+      .signOut()
+      .then(() => {
+        dispatch(setUser(null));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+}
+
+export function postArticleAPI(payload) {
+  return (dispatch) => {
+    if (payload.image != "") {
+      const upload = storage
+        .ref(`images/${payload.image.name}`)
+        .put(payload.image);
+      upload.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalbytes) * 100;
+
+          console.log(`Progress : $(progress)%`);
+          if (snapshot.stae === "RUNNING") {
+            console.log(`Progress : $(progress)%`);
+          }
+        },
+        (error) => console.log(error.code),
+        async () => {
+          const downloadURL = await upload.snapshot.ref.getDownloadURL();
+          db.collection("articles").add({
+            actor: {
+              description: payload.user.email,
+              title: payload.user.displayName,
+              date: payload.timestamp,
+              image: payload.user.photoURL,
+            },
+            video: payload.video,
+            sharedimage: downloadURL,
+            comments: 0,
+            description: payload.description,
+          });
+        }
+      );
+    }
   };
 }
